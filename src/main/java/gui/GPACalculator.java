9b2593621c960f3a30c184f226b58b6f55417110
Kevin <pidersk@gmail.com>
2022-01-28 12:01:17 -0500

    added some notification

--- src/main/java/gui/GPACalculator.java
+++ src/main/java/gui/GPACalculator.java
@@ -5,6 +5,9 @@ import calculator.Calculator;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +18,8 @@ public class GPACalculator {
     final double CollegeOthers = 1.0;
     final double Elective = 1.0;
     private Map<String, Double> Collection;
-    private ArrayList<Double> allGrades;
+    private Map<Integer, Double> Course;
+    private Map<Integer, Double> Credits;
     
     private JPanel root;
     private JButton getGPAButton;
@@ -28,36 +32,32 @@ public class GPACalculator {
     private JComboBox comboBox3;
     private JComboBox comboBox4;
     private JTextArea textArea1;
+    private JComboBox comboBox5;
+    private JComboBox comboBox6;
+    private JComboBox comboBox7;
+    private JComboBox comboBox8;
 
     public GPACalculator() {
-        this.allGrades = new ArrayList<>();
-        this.Collection = new HashMap<>();
-        this.Collection.put("A", 4.00);
-        this.Collection.put("A-",3.67);
-        this.Collection.put("B+",3.33);
-        this.Collection.put("B",3.00);
-        this.Collection.put("B-",2.67);
-        this.Collection.put("C+",2.33);
-        this.Collection.put("C",2.00);
-        this.Collection.put("C-",1.67);
-        this.Collection.put("D+",1.33);
-        this.Collection.put("D",1.00);
-        this.Collection.put("F",0.0);
+        initMaps();
         textArea1.setEditable(false);
         getGPAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(!isValid()){
-                    JOptionPane.showMessageDialog(root, "In valid grades, please check and enter again!");
+                    JOptionPane.showMessageDialog(root, "In valid grade(must be uppercase e.p.: B+) or didn't select a course type or credits. Please check and enter again!");
                     return;
                 }
 
+                double result = calculateResult();
+                result = round(result,2);
+                textArea1.setText(String.valueOf(result));
+                //result text Area
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GPACalculator");
+        JFrame frame = new JFrame("GPACalculator - LCDS TSA");
         frame.setContentPane(new GPACalculator().root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,300);
@@ -67,9 +67,8 @@ public class GPACalculator {
 
     private boolean isValid(){
         boolean v = true;
+
         if(!Collection.containsKey(textField1.getText())){
-            System.out.println(textField1.getText());
-            System.out.println(Collection.containsKey(textField1.getText()));
             v = false;
         }
         if(!Collection.containsKey(textField2.getText())){
@@ -81,6 +80,87 @@ public class GPACalculator {
         if(!Collection.containsKey(textField4.getText())){
             v = false;
         }
+        if(!Course.containsKey(comboBox1.getSelectedIndex())){
+            v = false;
+        }
+        if(!Course.containsKey(comboBox2.getSelectedIndex())){
+            v = false;
+        }
+        if(!Course.containsKey(comboBox3.getSelectedIndex())){
+            v = false;
+        }
+        if(!Course.containsKey(comboBox4.getSelectedIndex())){
+            v = false;
+        }
+        if(!Credits.containsKey(comboBox5.getSelectedIndex())){
+            v = false;
+        }
+        if(!Credits.containsKey(comboBox6.getSelectedIndex())){
+            v = false;
+        }
+        if(!Credits.containsKey(comboBox7.getSelectedIndex())){
+            v = false;
+        }
+        if(!Credits.containsKey(comboBox8.getSelectedIndex())){
+            v = false;
+        }
         return v;
+    }
+    private void initMaps(){
+        this.Collection = new HashMap<>();
+        this.Course = new HashMap<>();
+        this.Credits = new HashMap<>();
+
+        this.Collection.put("A", 4.00);
+        this.Collection.put("A-",3.67);
+        this.Collection.put("B+",3.33);
+        this.Collection.put("B",3.00);
+        this.Collection.put("B-",2.67);
+        this.Collection.put("C+",2.33);
+        this.Collection.put("C",2.00);
+        this.Collection.put("C-",1.67);
+        this.Collection.put("D+",1.33);
+        this.Collection.put("D",1.00);
+        this.Collection.put("F",0.0);
+
+        this.Course.put(3,1.15);
+        this.Course.put(2, 1.1);
+        this.Course.put(1,1.0);
+        this.Course.put(0,1.0);
+
+        this.Credits.put(0, 1.0);
+        this.Credits.put(1, 1.5);
+        this.Credits.put(2, 2.0);
+        this.Credits.put(3, 2.5);
+        this.Credits.put(4, 3.0);
+        this.Credits.put(5, 3.5);
+        this.Credits.put(6, 4.0);
+        this.Credits.put(7, 4.5);
+        this.Credits.put(8, 5.0);
+        this.Credits.put(9, 5.5);
+    }
+
+    private double calculateResult(){
+        double result = 0;
+        double credits = 0;
+        result += Collection.get(textField1.getText()) * Course.get(comboBox1.getSelectedIndex()) * Credits.get(comboBox5.getSelectedIndex());
+        result += Collection.get(textField2.getText()) * Course.get(comboBox2.getSelectedIndex()) * Credits.get(comboBox6.getSelectedIndex());
+        result += Collection.get(textField3.getText()) * Course.get(comboBox3.getSelectedIndex()) * Credits.get(comboBox7.getSelectedIndex());
+        result += Collection.get(textField4.getText()) * Course.get(comboBox4.getSelectedIndex()) * Credits.get(comboBox8.getSelectedIndex());
+        credits += Credits.get(comboBox5.getSelectedIndex());
+        credits += Credits.get(comboBox6.getSelectedIndex());
+        credits += Credits.get(comboBox7.getSelectedIndex());
+        credits += Credits.get(comboBox8.getSelectedIndex());
+        return result/credits;
+    }
+    private void reset(){
+
+    }
+    private double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
